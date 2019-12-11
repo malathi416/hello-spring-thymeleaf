c24e824c23f49ccff29ad18a48cb257900d8b34d
Malathi416 <ammu.416@gmail.com>
2019-12-11 10:12:16 -0600

    Completed dynamic thymeleaf template creation

--- src/main/java/org/launchcode/hellospringthymeleaf/controllers/HelloSpringThymeleafController.java
+++ src/main/java/org/launchcode/hellospringthymeleaf/controllers/HelloSpringThymeleafController.java
@@ -1,6 +1,7 @@
 package org.launchcode.hellospringthymeleaf.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -8,14 +9,16 @@ public class HelloSpringThymeleafController {
 
     //Responds to /hello?name=LaunchCode
     @RequestMapping(value = "hello",method = {RequestMethod.GET,RequestMethod.POST})
-    @ResponseBody
-    public String hello(@RequestParam String name){
-        return "Hello , " + name + "!";
+    public String hello(@RequestParam String name, Model model){
+        String greeting = "Hello , " + name + "!";
+        model.addAttribute("greeting",greeting);
+        return "hello";
     }
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloAgain(@PathVariable String name){
-        return "Hello , " + name + "!";
+    public String helloAgain(@PathVariable String name,Model model){
+        String greeting = "Hello , " + name + "!";
+        model.addAttribute("greeting",greeting);
+        return "hello";
     }
     @GetMapping("form")
     public String helloForm(){
